ff19bbfeb174bf0e00c6f1fb7f3ecf23dd7abdab
ckvv <k_0520@qq.com>
2023-03-16 14:01:24 +0800

    use createWebHashHistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import routes from '~pages';
 
 export const defaultRoutePath = '/home';
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [{
     path: '/',
     redirect: defaultRoutePath,
