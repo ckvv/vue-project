59ff32ca5e3b0b294028ad1f093fa0bdb1d7ccf0
ckvv <ckvv@users.noreply.github.com>
2023-05-18 08:31:45 +0000

    deploy: 4a6904b811e85fc2d703de0121d979ebe3dc626d

---
+++ assets/HelloWrold-322ebefb.js
@@ -0,0 +1,1 @@
+import{I as r}from"./index-54807870.js";const e={};function o(c,t){return" HelloWrold "}const _=r(e,[["render",o]]);export{_ as default};
--- assets/HelloWrold-bfcde489.js
+++
@@ -1,1 +0,0 @@
-const n=(t,e)=>{const o=t.__vccOpts||t;for(const[r,c]of e)o[r]=c;return o},_={};function s(t,e){return" HelloWrold "}const l=n(_,[["render",s]]);export{l as default};
