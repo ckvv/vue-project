87405994b5af2459ea4e2a3bb9c1b7f3f9fd5272
ckvv <ckvv@users.noreply.github.com>
2025-01-21 11:31:20 +0000

    deploy: 29c41d7e9b2b20c12c97f8a9f2526ee1eea9a4a4

--- assets/HelloWrold-CySF0qDS.js
+++
@@ -1,1 +0,0 @@
-import{L as r}from"./index-CNpFFyqq.js";const e={};function o(c,t){return" HelloWrold "}const _=r(e,[["render",o]]);export{_ as default};
---
+++ assets/HelloWrold-DW34sNBS.js
@@ -0,0 +1,1 @@
+import{L as r}from"./index-BVImx8lV.js";const e={};function o(c,t){return" HelloWrold "}const _=r(e,[["render",o]]);export{_ as default};
